ba8ad51b62171141a4a02cbd5be3f0c8f50f8f69
Jerome <ambroise.jerome.formation@gmail.com>
2019-06-17 15:24:30 +0200

    Routage variables

--- public/index.php
+++ public/index.php
@@ -7,8 +7,15 @@ use Psr\Http\Message\ServerRequestInterface;
 use Slim\App;
 use Slim\Http\Response;
 
+// Config
+$config = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ]
+];
+
 // On créé l'application Slim
-$app = new App();
+$app = new App($config);
 
 // Création d'une route
 $app->get('/homepage', function (ServerRequestInterface $request, ResponseInterface $response)
@@ -25,14 +32,43 @@ $app->get('/contact', function (ServerRequestInterface $request, ResponseInterfa
 
 $app->get('/hamac', function (ServerRequestInterface $request, Response $response)
 {
+    // Création des données
     $hamac = [
         "name" => "Hamac",
-        "description" => "Pour dormir APRES Slim"
+        "description" => "Pour dormir APRES Slim (pas pendant... ;))"
     ];
-
+    // On met les données dans la réponse (en précisant que ce doit être du JSON)
     $response = $response->withJson($hamac);
+    // On retourne la réponse au navigateur
     return $response;
 });
+
+// Page de la liste des produits
+$app->get(
+    '/produit/liste',
+    function(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        array $args
+    ) {
+        $response = $response->getBody()->write(
+            '<h1>Liste des produits</h1>'
+        );
+        return $response;
+    }
+);
+// Création d'une route possédant une variable
+$app->get(
+    '/produit/{id:\d+}',
+    function(ServerRequestInterface $request, ResponseInterface $response, array $args) {
+        // var_dump($args['id']);
+        $response = $response->getBody()->write(
+            "<h1>Détail du produit {$args['id']} </h1>"
+        );
+        return $response;
+    }
+);
+
 
 // Renvoi de la réponse au navigateur
 $app->run();

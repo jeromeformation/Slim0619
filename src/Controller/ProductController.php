5d8741138835b533436862967055d7d5a2f90385
Jerome <ambroise.jerome.formation@gmail.com>
2019-06-18 17:02:34 +0200

    Database.php - Recuperation produits de BDD - affichage liste produits en TWIG

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
 use App\Utilities\AbstractController;
+use App\Utilities\Database;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -10,7 +12,16 @@ class ProductController extends AbstractController
 {
     public function liste(ServerRequestInterface $request, ResponseInterface $response, array $args)
     {
-        return $this->twig->render($response, 'product/list.twig');
+        // Connexion à la BDD
+        $database = new Database();
+        // Requête SQL
+        $query = "SELECT * FROM produit WHERE etat_publication = 1";
+        // Exécution de la requête SQL et récupération des produits
+        $products = $database->query($query, Produit::class);
+        // On renvoie les produits à la vue
+        return $this->twig->render($response, 'product/list.twig', [
+            'products' => $products
+        ]);
     }
 
     public function show(ServerRequestInterface $request, ResponseInterface $response, array $args)
